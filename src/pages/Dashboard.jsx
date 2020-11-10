cbf266190f6d5aa6a86c8a806bd7111d7b7bf504
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2020-11-10 14:58:45 +0700

    AsdasD

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,13 +6,40 @@ export default class DashBoard extends React.Component {
     render(){
         return (
             <div className="dashboard-container">
-                This is the dashboard
-                <button className="btn-primary" onClick={_ => window.location.href ='/create'}>Create new gallery</button>
-                <div className="folder-container">
-                    <div style={{background:`url(${"https://res.cloudinary.com/yosuam19/image/upload/v1604903782/polaroid/Screenshot_2020-11-09_at_13.34.47_owxptp.png"})`}}  className="folder" >
-                    </div>
-                    <div className="folder-name">CNY Cakes</div>
+                <div className="dashboard-nav">
+                    <div className="dashboard-title">Dashboard</div>
+                    <button className="btn-primary" onClick={_ => window.location.href ='/create'}>Create new shelf</button>
                 </div>
+               
+                <div className="folder-section">
+                    <div className="folder-container">
+                        <div style={{background:`url(${"https://res.cloudinary.com/yosuam19/image/upload/v1604903782/polaroid/Screenshot_2020-11-09_at_13.34.47_owxptp.png"})`}}  className="folder" >
+                        </div>
+                        <div className="folder-name">CNY Cakes</div>
+                    </div>
+                    <div className="folder-container">
+                        <div style={{background:`url(${"https://res.cloudinary.com/yosuam19/image/upload/v1604903782/polaroid/Screenshot_2020-11-09_at_13.34.47_owxptp.png"})`}}  className="folder" >
+                        </div> 
+                        <div className="folder-name">CNY Cakes</div>
+                    </div>
+                    <div className="folder-container">
+                        <div style={{background:`url(${"https://res.cloudinary.com/yosuam19/image/upload/v1604903782/polaroid/Screenshot_2020-11-09_at_13.34.47_owxptp.png"})`}}  className="folder" >
+                        </div>
+                        <div className="folder-name">CNY Cakes</div>
+                    </div>
+                    <div className="folder-container">
+                        <div style={{background:`url(${"https://res.cloudinary.com/yosuam19/image/upload/v1604903782/polaroid/Screenshot_2020-11-09_at_13.34.47_owxptp.png"})`}}  className="folder" >
+                        </div>
+                        <div className="folder-name">CNY Cakes</div>
+                    </div>
+                    <div className="folder-container">
+                        <div style={{background:`url(${"https://res.cloudinary.com/yosuam19/image/upload/v1604903782/polaroid/Screenshot_2020-11-09_at_13.34.47_owxptp.png"})`}}  className="folder" >
+                        </div>
+                        <div className="folder-name">CNY Cakes</div>
+                    </div>
+
+                </div>
+                
                 
 
                 <div className="showcase"></div>
